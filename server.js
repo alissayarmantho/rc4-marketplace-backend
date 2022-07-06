fa953222e80aa4e89e4901d1976a03e436dbf9d1
alissayarmantho <alissayarmantho1@gmail.com>
2022-07-06 16:32:00 +0800

    Fix formatting

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const createError = require("http-errors");
-const logger = require("morgan"); //for logging in console, the request message
-const moment = require("moment-timezone");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const createError = require('http-errors');
+const logger = require('morgan'); //for logging in console, the request message
+const moment = require('moment-timezone');
 
-const apiRouter = require("./routes/api.routes");
+const apiRouter = require('./routes/api.routes');
 const app = express();
-const server = require("http").createServer(app);
+const server = require('http').createServer(app);
 
-moment.tz.setDefault("Asia/Singapore");
+moment.tz.setDefault('Asia/Singapore');
 
 app.use(cors());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 
 // parse requests of content-type - application/json
@@ -21,32 +21,32 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 
 // connect to DB
-const db = require("./models");
+const db = require('./models');
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("Connected to the database!");
+    console.log('Connected to the database!');
   })
   .catch((err) => {
-    console.log("Cannot connect to the database!", err);
+    console.log('Cannot connect to the database!', err);
     process.exit();
   });
 
 // simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to RC4 MarketPlace Backend Server REST API." });
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to RC4 MarketPlace Backend Server REST API.' });
 });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  console.log("404");
-  next(createError(404, "Invalid URL"));
+  console.log('404');
+  next(createError(404, 'Invalid URL'));
 });
 
 // error handler
@@ -54,7 +54,7 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   return res.json({
-    message: err + " is the error",
+    message: err + ' is the error',
     data: {},
   });
 });
